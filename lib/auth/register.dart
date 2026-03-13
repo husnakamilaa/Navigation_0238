4aa40d9f7cad98658364f7097316f0dd88337caf
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-03-13 12:17:58 +0700

    feat: inisialisasi stateful buat register dan menambahkan text controller (tempat nyimpen text sementara)

---
+++ lib/auth/register.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
