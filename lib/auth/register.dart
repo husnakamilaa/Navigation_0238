c41394ce463a9694aff2131968308dc01c8d5c3d
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-03-13 14:05:34 +0700

    feat: menambahkan password field beserta validasi dan menambahkan jarak height untuk antar textfield

--- lib/auth/register.dart
+++ lib/auth/register.dart
@@ -145,7 +145,37 @@ class _RegisterPageState extends State<RegisterPage> {
                       },
                     ),
                     const SizedBox(height: 16),
-                    
+                    TextFormField(
+                      controller: passwordController,
+                      obscureText: true,
+                      textInputAction: TextInputAction.done,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        labelStyle: const TextStyle(color: MainLayout.labelColor),
+                        prefixIcon: const Icon(Icons.person_outline,
+                        color: MainLayout.primaryColor),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                          borderSide: const BorderSide(
+                            color: MainLayout.inputBorderColor),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                          borderSide: const BorderSide(
+                            color: MainLayout.inputBorderColor, width: 2),
+                        ),
+                        filled: true,
+                        fillColor: MainLayout.inputFillColor,
+                      ),
+                      validator: (value) {
+                        if(value == null || value.isEmpty){
+                          return 'Please enter yout password';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 32),
+
                   ],
                 ),
               ),
